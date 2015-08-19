0de10a6ed359d24179a99c7b87e1acf4022bbaeb
sahmes <sahmes@gmail.com>
2015-08-19 19:48:44 +0800

    Now i/o is HDF5.
    Need to make switches to allow ASCII i/o.

--- src/io.cpp
+++ src/io.cpp
@@ -15,7 +15,11 @@
 
 using namespace std;
 
+void ReadICsHDF5(string Filename, int N, Particle **P_h, int *FileSnapshotNum, Real *FileTime);
+
 void ReadICs(string Filename, int N, Particle **P_h, int *FileSnapshotNum, Real *FileTime) {
+    ReadICsHDF5("IC.h5part", N, P_h, FileSnapshotNum, FileTime);
+    return;
     *P_h = new Particle[N];
     string Line;
     ifstream InputFile(Filename.c_str());
@@ -77,10 +81,10 @@ void ReadICs(string Filename, int N, Particle **P_h, int *FileSnapshotNum, Real 
     }
 }
 
-void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T);
+void WriteSnapshotHDF5(string Prefix, int SnapNumber, Particle *P_h, int N, Real T);
 void WriteSnapshot(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
-//     Dooooo(Prefix, SnapNumber, P_h, N, T);
-//     return;
+    WriteSnapshotHDF5(Prefix, SnapNumber, P_h, N, T);
+    return;
     char S[512];
     sprintf(S, "%s%04d", Prefix.c_str(), SnapNumber);
     ofstream SnapshotFile;
@@ -169,14 +173,120 @@ void ParseInput(int argc, char *argv[], ParametersStruct *Params) {
 // make safety: if single precision, fail to compile
 #ifdef ETICS_HDF5
 #include "H5Cpp.h"
-void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
-    
-    char Filename[512];
-    sprintf(Filename, "%s.h5part", Prefix.c_str());
 
-    
-//     WriteSnapshot(string Prefix, int SnapNumber, Particle *P_h, int N, Real T)
-    double *X, *Y, *Z, *VX, *VY, *VZ, *AX, *AY, *AZ;
+int H5IterNum;
+int *H5IterSnapNumbers;
+string *H5IterGroupNames;
+
+herr_t file_info(hid_t loc_id, const char *name, void *opdata) {
+    int res = sscanf(name, "Step#%d", H5IterSnapNumbers + H5IterNum);
+    if (res != 1) {
+        cerr << "Problem understanding group \"" << name << "\" in HDF5 file" << endl;
+        exit(1);
+    }
+    H5IterGroupNames[H5IterNum] = name;
+    H5IterNum++;
+    return 0;
+}
+
+
+void ReadICsHDF5(string Filename, int N, Particle **P_h, int *FileSnapshotNum, Real *FileTime) {
+    double *Mass, *X, *Y, *Z, *VX, *VY, *VZ;
+    int *ID;
+    Mass  = new double[N];
+    X  = new double[N];
+    Y  = new double[N];
+    Z  = new double[N];
+    VX = new double[N];
+    VY = new double[N];
+    VZ = new double[N];
+    ID = new int[N];
+
+    H5::H5File file;
+    file = H5::H5File(Filename, H5F_ACC_RDONLY);
+
+    H5::Group group = file.openGroup("/");
+    int NumberOfGroups = group.getNumObjs();
+    H5IterSnapNumbers = new int[NumberOfGroups];
+    H5IterGroupNames = new string[NumberOfGroups];
+    H5IterNum = 0;
+    int ret = file.iterateElems("/", NULL, file_info, NULL);
+    int MaxSnapIndex = 0;
+    for (int i=1; i < NumberOfGroups; i++) MaxSnapIndex = (H5IterSnapNumbers[i]>H5IterSnapNumbers[MaxSnapIndex])?i:MaxSnapIndex;
+    string GroupName = H5IterGroupNames[MaxSnapIndex];
+
+
+    group = file.openGroup("/" + GroupName);
+    H5::Attribute attribute = group.openAttribute("Time");
+    attribute.read(H5::PredType::NATIVE_DOUBLE, FileTime);
+    *FileSnapshotNum = H5IterSnapNumbers[MaxSnapIndex];
+
+    H5::DataSet dataset = file.openDataSet("/" + GroupName + "/ID");
+    int NfromFile = dataset.getStorageSize()/sizeof(int);
+    if (NfromFile < N) {
+        cerr << "Was only able to read " << NfromFile << " particles while " << N << " were requested." << endl;
+        exit(1);
+    }
+    dataset.read(ID, H5::PredType::NATIVE_UINT32);
+
+    dataset = file.openDataSet("/" + GroupName + "/Mass");  dataset.read(Mass,  H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/X");     dataset.read(X,  H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/Y");     dataset.read(Y,  H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/Z");     dataset.read(Z,  H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/VX");    dataset.read(VX, H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/VY");    dataset.read(VY, H5::PredType::NATIVE_DOUBLE);
+    dataset = file.openDataSet("/" + GroupName + "/VZ");    dataset.read(VZ, H5::PredType::NATIVE_DOUBLE);
+
+    dataset.close();
+    group.close();
+    file.close();
+
+    *P_h = new Particle[N];
+    for (int i = 0; i < N; i++) {
+        Particle p;
+        p.ID = ID[i];
+        p.m = (Real)Mass[i];
+        p.pos = vec3((Real)X[i], (Real)Y[i], (Real)Z[i]);    // This is to ensure proper casting in the case of single prec.
+        p.vel = vec3((Real)VX[i], (Real)VY[i], (Real)VZ[i]);
+        p.Status = 0;
+        p.CalculateR2();
+        (*P_h)[i] = p;
+    }
+}
+
+bool H5FirstSnapshot = true;
+
+void WriteSnapshotHDF5(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
+    string Filename = Prefix + ".h5part";
+
+    H5::H5File file;
+
+    if (H5FirstSnapshot && std::ifstream(Filename.c_str())) {
+        cerr << "File \"" << Filename << "\" already exist!" << endl;
+        exit(1);
+    }
+    H5FirstSnapshot = false;
+
+    if (std::ifstream(Filename.c_str())) {
+        file = H5::H5File(Filename, H5F_ACC_RDWR);
+    }
+    else file = H5::H5File(Filename, H5F_ACC_TRUNC);
+
+    char GroupName[64];
+    sprintf(GroupName, "/Step#%d", SnapNumber);
+
+    H5::Group group;
+    try {
+        H5::Exception::dontPrint();
+        group = H5::Group(file.createGroup(GroupName));
+    }
+    catch (  H5::FileIException error ) {
+        cerr << "Couldn't create group \"" << GroupName << "\" in HDF5 file, maybe it already exists?"  << endl;
+        exit(1);
+    }
+
+    double *Mass, *X, *Y, *Z, *VX, *VY, *VZ, *AX, *AY, *AZ;
+    Mass  = new double[N];
     X  = new double[N];
     Y  = new double[N];
     Z  = new double[N];
@@ -192,6 +302,7 @@ void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
     for (int i = 0; i < N; i++) {
         Particle p = P_h[i];
         ID[i] = p.ID;
+        Mass[i]  = p.m;
         X[i]  = p.pos.x;
         Y[i]  = p.pos.y;
         Z[i]  = p.pos.z;
@@ -202,21 +313,6 @@ void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
         AY[i] = p.acc.y;
         AZ[i] = p.acc.z;
     }
-
-    H5::H5File file;
-    std::ifstream infile(Filename);
-    if (!infile.good()) file = H5::H5File(Filename, H5F_ACC_TRUNC);
-    else file = H5::H5File(Filename, H5F_ACC_RDWR);
-#warning be more sophosticated when checking if file exists. Open it and if the snapshot we are trying to write already exist, then fail.
-    
-#warning also make it so when we read the ini file, if the ic file ends with .h5part, then we treat it as continue.
-    
-#warning you forgot to include the mass in the parameters
-
-    char GroupName[64];
-    sprintf(GroupName, "/Step#%d", SnapNumber);
-
-    H5::Group group(file.createGroup(GroupName));
 
     H5::DataSpace attr_dataspace = H5::DataSpace(H5S_SCALAR);
 
@@ -231,6 +327,8 @@ void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
     H5::DataSet dataset3;
     dataset3 = H5::DataSet(group.createDataSet("ID", H5::PredType::NATIVE_UINT32, dataspacexxx));
     dataset3.write(ID, H5::PredType::NATIVE_UINT32);
+    dataset3 = H5::DataSet(group.createDataSet("Mass",  H5::PredType::NATIVE_DOUBLE, dataspacexxx));
+    dataset3.write(Mass, H5::PredType::NATIVE_DOUBLE);
     dataset3 = H5::DataSet(group.createDataSet("X",  H5::PredType::NATIVE_DOUBLE, dataspacexxx));
     dataset3.write(X, H5::PredType::NATIVE_DOUBLE);
     dataset3 = H5::DataSet(group.createDataSet("Y",  H5::PredType::NATIVE_DOUBLE, dataspacexxx));
@@ -250,7 +348,6 @@ void Dooooo(string Prefix, int SnapNumber, Particle *P_h, int N, Real T) {
     dataset3 = H5::DataSet(group.createDataSet("AZ", H5::PredType::NATIVE_DOUBLE, dataspacexxx));
     dataset3.write(AZ, H5::PredType::NATIVE_DOUBLE);
 
-    
     dataset3.close();
     dataspacexxx.close();
 
